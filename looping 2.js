a633e858c15c4515feb17c2314868cca2f3586be
Elijah2Hill <90879350+Elijah2Hill@users.noreply.github.com>
2021-09-26 18:30:32 -0400

    Two New Loop Files

---
+++ looping 2.js
@@ -0,0 +1,24 @@
+// FOR LOOP logs from 0 to 1000
+let count = 1000;
+while(count < 0) {
+    console.log(count);
+    count++;
+}
+alert("A quarter of the way there!");
+alert("Halfway there!");
+alert("The loop is done!");
+
+// FOR LOOP logs from 0 to 1000
+for(let i = 1000; i >= 0; i -= 2) {
+    console.log(i);
+}
+
+// FOR LOOP - TV show version
+for(let i = 0; i >= 100; i++) {
+    console.log(i);
+}
+console.log("My #1 favorite TV show is Phineas and Ferb");
+console.log("My #2 favorite TV show is Dexter's Laboratory");
+console.log("My #3 favorite TV show is Mickey Mouse Clubhouse");
+console.log("My #4 favorite TV show is Special Agent OSO");
+console.log("My #5 favorite TV show is Drake and Josh");
