f6db3b2dfc7f2665bd386f26a4a08a21244b03f0
Elijah2Hill <90879350+Elijah2Hill@users.noreply.github.com>
2022-02-20 17:58:12 -0500

    Update looping 2.js

--- looping 2.js
+++ looping 2.js
@@ -1,24 +1,16 @@
-// FOR LOOP logs from 0 to 1000
-let count = 1000;
-while(count < 0) {
-    console.log(count);
-    count++;
-}
-alert("A quarter of the way there!");
-alert("Halfway there!");
-alert("The loop is done!");
+for (i = 1000, i >= 0, i -= 2)
+	console.log(i);
 
-// FOR LOOP logs from 0 to 1000
-for(let i = 1000; i >= 0; i -= 2) {
-    console.log(i);
+for (i = 0, i <= 10000, i++) {
+	if (i == 2500)
+		alert("A quarter of the way there!");
+	if (i == 5000)
+		alert("Halfway there!");
+	if (i == 10000)
+		alert("The loop is done!");
 }
 
-// FOR LOOP - TV show version
-for(let i = 0; i >= 100; i++) {
-    console.log(i);
+const tv_show = ["Grand Sumo Highlights", "Easy Japanese for Work", "Four Seasons in Japan", "Songs of Tokyo", "Anime Supernova"];
+for (i = 0, i < tv_show.length, i++) {
+	console.log("My #" + (i+1) + " favorite tv show is " + tv_show[i]);
 }
-console.log("My #1 favorite TV show is Phineas and Ferb");
-console.log("My #2 favorite TV show is Dexter's Laboratory");
-console.log("My #3 favorite TV show is Mickey Mouse Clubhouse");
-console.log("My #4 favorite TV show is Special Agent OSO");
-console.log("My #5 favorite TV show is Drake and Josh");
