adecb964fef4a14ba1ff497e53c750ccd953386b
Elijah2Hill <90879350+Elijah2Hill@users.noreply.github.com>
2022-02-20 19:14:27 -0500

    Update looping 2.js

--- looping 2.js
+++ looping 2.js
@@ -1,7 +1,7 @@
-for (i = 1000; i >= 0; i -= 2)
+for (let i = 1000; i >= 0; i -= 2)
 	console.log(i);
 
-for (i = 0; i <= 10000; i++) {
+for (let i = 0; i <= 10000; i++) {
 	if (i == 2500)
 		alert("A quarter of the way there!");
 	if (i == 5000)
@@ -11,6 +11,6 @@ for (i = 0; i <= 10000; i++) {
 }
 
 const tv_show = ["Grand Sumo Highlights", "Easy Japanese for Work", "Four Seasons in Japan", "Songs of Tokyo", "Anime Supernova"];
-for (i = 0; i < tv_show.length; i++) {
+for (let i = 0; i < tv_show.length; i++) {
 	console.log("My #" + (i+1) + " favorite tv show is " + tv_show[i]);
 }
